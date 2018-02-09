fdf5ac2e96561ed7e961f41eed1b812cf32ef315
Bruno <alunos@intranet.lan>
2018-02-08 22:26:01 -0200

    primeira aula

---
+++ JavaApplication5/src/ReadFile.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+
+public class ReadFile
+{
+    public static void main(String[]args) throws FileNotFoundException, IOException
+    {
+        InputStream inputStream = new FileInputStream("arquivo.txt");
+        
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        
+        BufferedReader bufferReader = new BufferedReader(inputStreamReader);
+        
+        //int i = inputStream.read();
+        
+        
+        String line;
+        while((line = bufferReader.readLine()) != null)
+        {
+            System.out.println(line);
+        }
+        
+    }
+}
